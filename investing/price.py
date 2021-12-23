a1d62c7513937b38e363825dbc3fb45394773172
Ben Kogan <brk57@cornell.edu>
2021-12-23 18:07:03 -0500

    scrape price

---
+++ investing/price.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def get_price(ticker):
+    url = f"https://www.marketwatch.com/investing/stock/{ticker}"
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    try:
+        results = soup.find_all(class_="intraday__price")
+        price = float(re.findall("\d*\.?\d+", results[0].text)[0])
+    except Exception:
+        price = None
+
+    return price
